fade853ea71ccdeaf7951ec70a23231079c433c8
IbanPlay <56191483+IbanPlay@users.noreply.github.com>
2022-05-28 07:17:30 +0200

    Boi but OOP

---
+++ Boi/BaseClasses/BoiHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+
+namespace CalValEX.Boi.BaseClasses
+{
+	public class BoiHandler
+	{
+		public static Vector2 playingField = new Vector2(500, 500);
+		public List<BoiRoom> Map;
+		public List<BoiEntity> Entities;
+
+		public void Run()
+        {
+			//Process the players movement and actions
+
+
+			//For each entity
+			//-Run their update function (and the UpdateEffect functions of their inventory slots
+			//-If they either are not an IColliding, or are one but have CanCollide set to false, update their position based on their current velocity. If they aren't either, add them to a list
+			//-After updating their position, check if they are still within the bounds of the playing field. If not, either clamp their position to it, or kill them (depends on if their DestroyOnRoomEdgeContact variable is set to true or not) (if it is, call the EdgeContactDestroy method)
+
+			//-If IDamageable & Vulnerable is true, store them in a list. Additioanlly, get a dictionnary that pairs them with their hurtbox
+			//-If IDamageDealer, store them in a list
+			//-If IInteractable, store them in a list
+			//-You get the idea. Do that for all, but the Idrawable
+
+			//For each IDamageDealer run the HitCheck function on all IDamageable hurtboxes that can are of a faction that they are hostile to. 
+			//If the HitCheck returns true, run the DealDamage function to get the damage dealt by them.
+			//Call the DamageTaken function of any of the IDamageable entity that were hit
+			//If their health is now under zero, run the Die function, then clear them from the Entities list, unless the function returns true. In which case, check if their health is under zero again (until either they have more than zero hp, or the function returns false)
+
+			//For each entity that is IColliding and has CanCollide set to true (as listed above)
+			//Check for all IColliders, and grab their SimulationDistance.
+			//If the simulation distance + the CollisionCircleRadius of the IColliding is higher than the distance between the two entities, thats awesome, don't even do anything about it and go to the next one
+			//If it ISNT, call the MovementCheck function and provide the position of the entity with their current velocity added
+			//If the distance obtained from the MovementCheck function added to the CollisionCircleRadius is lower than the distance between the two entities, call both their onCollide function, and cancel the movement of the IColliding
+
+			//If all the collisions checks passed, update their position based on their current velocity. Do the same checks about the room position and everything
+
+			//For each IInteractable, check if the player is in their InteractionRadius and if they have CanBeInteracted to true. If it is the case, simply just run their Interact() function.
+			//Kill the interactable if the Interact function returns true
+
+
+			//Clear all the lists created earlier
+		}
+
+		public void Draw()
+        {
+			//Draw bg
+			//Draw doors and such
+			//For each entity in Entities, if its an IDrawable, store them in new lists, separated on their Layer
+			//Then for all these lists, draw the IDrawables with the proper scale and offset.
+        }
+	}
+}
---
+++ Boi/BaseClasses/Interfaces.cs
@@ -0,0 +1,226 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ModLoader;
+using System.Collections.Generic;
+
+namespace CalValEX.Boi.BaseClasses
+{
+
+    public class BoiEntity
+    {
+        public Vector2 Position;
+        public Vector2 Velocity;
+        public List<BoiItem> Inventory;
+
+        public bool DestroyOnRoomEdgeContact => false;
+        public void EdgeContactDestroy() { };
+
+        public virtual void OnSpawn() { }
+        public virtual void Update() { }
+    }
+
+    public class BoiRoom
+    {
+
+    }
+
+    public class BoiItem
+    {
+        Entity Owner;
+
+        public void UpdateEffect() { }
+    }
+
+    public class BoiPlayer : BoiEntity
+    {
+        public static float InteractionRadius = 5;
+
+        public void ProcessControls()
+        {
+            return;
+        }
+    }
+
+    public enum Factions
+    {
+        ally,
+        enemy
+    }
+
+    /// <summary>
+    /// Used for any entity that can deal damage, such as bullets or enemies with contact damage.
+    /// </summary>
+    public interface IDamageDealer
+    {
+        /// <summary>
+        /// What faction is this entity able to deal damage to?
+        /// </summary>
+        public Factions[] hostileTo
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Checks if for any given active hurtbox, this entity collides with it.
+        /// </summary>
+        /// <param name="hurtbox">The hurtbox of the entity you're checking if you're hittign</param>
+        /// <returns>Wether or not a collision happened</returns>
+        public bool HitCheck(Rectangle hurtbox);
+
+        /// <summary>
+        /// Returns the amount of damage this should deal
+        /// You can use this function to add extra on-hit effects as well.
+        /// Do not reduce the target's health in this method though. This is handled automatically
+        /// </summary>
+        /// <returns>The damage dealt</returns>
+        public float DealDamage(BoiEntity target);
+    }
+
+    /// <summary>
+    /// Used for any entity that can take damage.
+    /// </summary>
+    public interface IDamageable
+    {
+        /// <summary>
+        /// The health of the entity
+        /// </summary>
+        public float Health 
+        {
+            get;        
+        }
+
+        /// <summary>
+        /// What faction does this entity belong to?
+        /// </summary>
+        public Factions Faction
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Gets the hurtbox of the entity
+        /// </summary>
+        public Rectangle Hurtbox
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Can the entity be hit currently?
+        /// </summary>
+        public bool Vulnerable => true;
+
+        /// <summary>
+        /// Called when the entity gets hit. Use for any on hit effects.
+        /// </summary>
+        public void TakeHit(float damageTaken) { }
+
+        /// <summary>
+        /// Called when the entity hits zero health.
+        /// Return true if the entity's health should be checked again after this method is called. Use this if you want ressurection effects / Death prevention.
+        /// </summary>
+        public bool Die() { return false; }
+    }
+
+    /// <summary>
+    /// Any entity that can prevent movement
+    /// The hitbox should be convex. If you need concave shapes , use more convex objects
+    /// </summary>
+    public interface ICollidable
+    {
+        /// <summary>
+        /// This should represent the longest distance a point can be from the center of this object and still collide with it.
+        /// E.G, a round rock of radius 1 would return 1. A square tile would return the value of one half its diagonal, since the distance between the center and the corner of a square is the longest distance
+        /// </summary>
+        public float SimulationDistance
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Returns the distance between the object's surface and a potentially colliding entity.
+        /// Example : This would return 0.5 if the object is a sphere of radius 1, and the potentially colliding entity was 1.5 units of distance from the spheres center.
+        /// </summary>
+        /// <param name="position">The position of the potentially colliding entity</param>
+        /// <returns></returns>
+        public float MovementCheck(Vector2 position);
+
+
+        /// <summary>
+        /// What happens on a collision with an entity. If you want to hurt them on contact though, it would be wiser to implement an IDamageDealer interface to the object.
+        /// </summary>
+        /// <param name="collider">What entity collided with this object</param>
+        public void OnCollide(BoiEntity collider) { }
+
+    }
+
+    /// <summary>
+    /// Represents an entity that can collide with collidable objects.
+    /// </summary>
+    public interface IColliding
+    {
+        /// <summary>
+        /// How big the radius of collision is for this entity.
+        /// </summary>
+        public float CollisionCircleRadius
+        {
+            get;
+        }
+
+        /// <summary>
+        /// If the collision of this object is currently on. If the entity just never collides, please simply don't implement this interface uwu
+        /// </summary>
+        public bool CanCollide
+        {
+            get;
+        }
+
+        /// <summary>
+        /// What happens on a collision.
+        /// </summary>
+        public void OnCollide(BoiEntity collider) { }
+    }
+
+    /// <summary>
+    /// Represents an object the player can interact with
+    /// </summary>
+    public interface IInteractable
+    {
+        /// <summary>
+        /// How big the radius of collision is for this interactable.
+        /// </summary>
+        public float CollisionCircleRadius
+        {
+            get;
+        }
+
+        /// <summary>
+        /// Can the player interact with this?
+        /// Useful if you want certain items to only be pickable up with a keybind.
+        /// </summary>
+        public bool CanBeInteractedWith => true;
+
+        /// <summary>
+        /// What happens when the item gets interacted with
+        /// Return true to kill the interactable after the itneraction
+        /// </summary>
+        /// <param name="player">The player that interacted with the entity</param>
+        public bool Interact(BoiPlayer player);
+    }
+
+    /// <summary>
+    /// Represents an entity that can be drawn
+    /// </summary>
+    public interface IDrawable
+    {
+
+        /// <summary>
+        /// The layer this should be drawn at. 
+        /// Higher number = gets drawn above everything else on a lower layer.
+        /// </summary>
+        public int Layer => 0;
+
+        public void Draw(SpriteBatch spriteBatch, Vector2 offset, float scale);
+    }
+}
